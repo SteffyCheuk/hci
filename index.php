39faebbf69ae716be212ef6dd037c40c85ec037d
Stepheny Cheuk <sc2354@cornell.edu>
2015-11-29 18:10:31 -0500

    fixed string

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ ini_set('display_startup_errors', TRUE);
 # This function reads your DATABASE_URL configuration automatically set by Heroku
 # the return value is a string that will work with pg_connect
 function pg_connection_string() {
-  return "psql: dbname=d2ffsrfmtpiq4e host=ec2-54-83-202-218.compute-1.amazonaws.com port=5432 user=pcxjnmoniwdgvr password=b7NGpwu_ELAol4LuUL3AcvoXN_ sslmode=require";
+  return "dbname=d2ffsrfmtpiq4e host=ec2-54-83-202-218.compute-1.amazonaws.com port=5432 user=pcxjnmoniwdgvr password=b7NGpwu_ELAol4LuUL3AcvoXN_ sslmode=require";
 }
  
 # Establish db connection
