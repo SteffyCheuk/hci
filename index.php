caf36a03f72bfe6898b3a3ceebcab305d000b521
Stepheny Cheuk <sc2354@cornell.edu>
2015-11-29 17:36:45 -0500

    debugging...

--- index.php
+++ index.php
@@ -11,8 +11,10 @@ The content of the document......
 </html> 
 
 <?php
-ini_set('display_errors', 'On');
 error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
 # This function reads your DATABASE_URL configuration automatically set by Heroku
 # the return value is a string that will work with pg_connect
 function pg_connection_string() {
