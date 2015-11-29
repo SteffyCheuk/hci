c9cf9bad7ef041bf83e12f4ce1868acb692d0e2f
Stepheny Cheuk <sc2354@cornell.edu>
2015-11-29 18:26:04 -0500

    database

--- database.php
+++ database.php
@@ -11,65 +11,72 @@ if (!$db) {
   echo "Database connection error.";
 }
  
-$setup_db = "
+$setup_users = "
+  CREATE TABLE users (
+    id              INT         PRIMARY KEY,
+    first_name      TEXT,
+    last_name       TEXT,
+    type            CHAR(20),
+    email           CHAR(50),
+    phone           CHAR(10)
+  );";
 
-CREATE TABLE users (
-  id              INT         PRIMARY KEY,
-  first_name      TEXT,
-  last_name       TEXT,
-  type            CHAR(20),
-  email           CHAR(50),
-  phone           CHAR(10)
-);
+$setup_tasks = " 
+  CREATE TABLE tasks (
+    id              INT         PRIMARY KEY,    
+    older_adult_id  INT,
+    owner_id        INT,
+    title           TEXT,
+    description     TEXT,
+    location        TEXT,
+    deadline        TIMESTAMP,
+    done            BOOLEAN,
+    notified        BOOLEAN
+  );";
 
-CREATE TABLE tasks (
-  id              INT         PRIMARY KEY,    
-  older_adult_id  INT,
-  owner_id        INT,
-  title           TEXT,
-  description     TEXT,
-  location        TEXT,
-  deadline        TIMESTAMP,
-  done            BOOLEAN,
-  notified        BOOLEAN
-);
+$setup_relationships = "
+  CREATE TABLE relationships (
+    id              INT         PRIMARY KEY,
+    caregiver_id    INT,
+    older_adult_id  INT,
+    permissions     BOOLEAN
+  );";
 
-CREATE TABLE relationships (
-  id              INT         PRIMARY KEY,
-  caregiver_id    INT,
-  older_adult_id  INT,
-  permissions     BOOLEAN
-);
+$setup_locations = "
+  CREATE TABLE locations (
+    id              INT         PRIMARY KEY,
+    older_adult_id  INT,
+    date            DATE,
+    time            TIME,
+    location        TEXT,
+    long            DECIMAL,
+    lat             DECIMAL,
+  );";
 
-CREATE TABLE locations (
-  id              INT         PRIMARY KEY,
-  older_adult_id  INT,
-  date            DATE,
-  time            TIME,
-  location        TEXT,
-  long            DECIMAL,
-  lat             DECIMAL,
-);
+$setup_saved_locations = "
+  CREATE TABLE saved_locations (
+    id              INT         PRIMARY KEY,
+    older_adult_id  INT,
+    name            TEXT,
+    location        TEXT,
+    long            DECIMAL,
+    lat             DECIMAL
+  );";
 
-CREATE TABLE saved_locations (
-  id              INT         PRIMARY KEY,
-  older_adult_id  INT,
-  name            TEXT,
-  location        TEXT,
-  long            DECIMAL,
-  lat             DECIMAL
-);
+$setup_settings = "
+  CREATE TABLE settings (
+    id              INT         PRIMARY KEY,
+    user_id         INT,
+    font_size       TEXT,
+    language        TEXT,
+    sound           INT
+  );";
 
-CREATE TABLE settings (
-  id              INT         PRIMARY KEY,
-  user_id         INT,
-  font_size       TEXT,
-  language        TEXT,
-  sound           INT
-);
-
-";
-
-$result = pg_query($db, $setup_db);
+$result = pg_query($db, $setup_users);
+$result = pg_query($db, $setup_tasks);
+$result = pg_query($db, $setup_relationships);
+$result = pg_query($db, $setup_locations);
+$result = pg_query($db, $setup_saved_locations);
+$result = pg_query($db, $setup_settings);
 
 ?>
