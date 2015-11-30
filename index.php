edd10b51726592f199d0fb3a1eeca31721bab0e3
Stepheny Cheuk <sc2354@cornell.edu>
2015-11-29 21:23:16 -0500

    form styling

--- index.php
+++ index.php
@@ -19,7 +19,6 @@
           header("Location: tasks.php");
         }
       ?>
-      <?php include "partials/navigation.php"; ?>
 
     </div>
   </body>
--- partials/login.php
+++ partials/login.php
@@ -1,13 +1,15 @@
-<form id="login" name="login" method="post" action="tasks.php">
-  Email: <br/>
-  <input id="email" type="text"> 
-  <br/>
-  Password: <br/>
-  <input id="password" type="password">
-  <br/>
-  <input class="button" type="submit" value="submit">
-</form>
+<div id="form">
+  <form id="login" name="login" method="post" action="tasks.php">
+    Email: <br/>
+    <input id="email" type="text"> 
+    <br/>
+    Password: <br/>
+    <input id="password" type="password">
+    <br/>
+    <input class="button" type="submit" value="submit">
+  </form>
 
-<form id="register" name="register" action="register.php">
-  <input class="button" type="submit" vallue="register">
-</form>
+  <form id="register" name="register" action="register.php">
+    <input class="button" type="submit" value="register">
+  </form>
+</div>
