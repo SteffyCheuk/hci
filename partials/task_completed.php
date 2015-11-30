9900263eb950c91b992247da2008d749409f9b33
Stepheny Cheuk <sc2354@cornell.edu>
2015-11-30 16:45:30 -0500

    implemented deletion

--- partials/task_completed.php
+++ partials/task_completed.php
@@ -2,7 +2,7 @@
   require_once "DB.php";
     
   if (isset($_POST["id"])){
-    $id = $_POST["id"]
+    $id = $_POST["id"];
     $done = $_POST["checked"]; 
     $sql = "UPDATE tasks SET done = {$done} WHERE id = {$id};";
     $result = pg_query($db, $sql);
