cb71bcd71b1eeb864de6c9aca18e59557b60e270
Stepheny Cheuk <sc2354@cornell.edu>
2015-11-29 22:25:05 -0500

    style

--- register.php
+++ register.php
@@ -11,27 +11,26 @@
     <div id="container">
 
       <?php require_once "partials/DB.php"; ?>
-      <div id="register-form">
-        <form id="registration" name="registration" method="post" action="tasks.php">
-          <h3>Register</h3>
-          <section>
-            <input type="radio" name="type" value="Caregiver">
-            <input type="radio" name="type" value="Older Adult">
-          </section>
-          <h3>Details</h3>
-          <section>
-            <input type="text" name="first-name">
-            <input type="text" name="last-name">
-            <input type="text" name="email">
-            <input type="password" name="password">
-            <input type="password" name="password-validation">
-          </section>
-          <h3>Emergy Contacts</h3>
-          <section>
-            <input type="text" name="phone">
-          </section>
-        </form>
-      </div>
+      <!-- REGISTRATION WIZARD FORM -->
+      <form id="registration" name="registration" method="post" action="tasks.php">
+        <h3>Register</h3>
+        <section>
+          <input type="radio" name="type" value="Caregiver">
+          <input type="radio" name="type" value="Older Adult">
+        </section>
+        <h3>Details</h3>
+        <section>
+          <input type="text" name="first-name">
+          <input type="text" name="last-name">
+          <input type="text" name="email">
+          <input type="password" name="password">
+          <input type="password" name="password-validation">
+        </section>
+        <h3>Emergy Contacts</h3>
+        <section>
+          <input type="text" name="phone">
+        </section>
+      </form>
       <?php 
         
       ?>
