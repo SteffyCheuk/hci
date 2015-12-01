545c81bf346b4f0bd0639afbe09e01184dfe67dc
Mark Yoon <mark.youngho.yoon@gmail.com>
2015-12-01 00:33:04 -0500

    Small slider fixes

--- map.php
+++ map.php
@@ -5,9 +5,6 @@
     <title>M.A.P. - Memos and Protection </title>
     <link rel="stylesheet" href="./assets/style.css">
     <link rel="stylesheet" href="./assets/map.css">
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 
     <script type="text/javascript">
 
@@ -15,7 +12,7 @@
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 42.442232, lng: -76.489213},
-          zoom: 15
+          zoom: 17
         });
 
         var marker = new google.maps.Marker({
@@ -24,16 +21,10 @@
           title: 'Hello World!'
         });
       }
-
     </script>
+
     <script async defer
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyArpx7Ru9n10wbmsOClbGeBGkdn0SY-3MI&callback=initMap">
-    </script>
-
-    <script>
-    $(function() {
-      $( "#slider" ).slider();
-    });
     </script>
 
   </head>
@@ -44,7 +35,24 @@
       <?php require_once "partials/DB.php"; ?>
 
     <div id="map"></div>
-    <div id="slider"></div>
+    <div id="settings">
+      <input id='slider' type="range" min="0" max="120" step="10" list="steplist">
+      <datalist id="steplist">
+          <option>0</option>
+          <option>10</option>
+          <option>20</option>
+          <option>30</option>
+          <option>40</option>
+          <option>50</option>
+          <option>60</option>
+          <option>70</option>
+          <option>80</option>
+          <option>90</option>
+          <option>100</option>
+          <option>110</option>
+          <option>120</option>
+      </datalist>
+    </div>
 
       <!-- MAIN CONTENT GOES HERE -->
 
