85f53df22299b2aa96f3d5d2be1b73424e07e483
Stepheny Cheuk <sc2354@cornell.edu>
2015-11-30 19:05:17 -0500

    settings

--- settings.php
+++ settings.php
@@ -7,10 +7,31 @@
 
   <body>
     <div id="container">
-    
       <?php require_once "partials/DB.php"; ?>
+      <?php include "partials/header.php"; ?>
+      <?php 
+        session_start();
+        $_SESSION['user'] = 1;
+        if (isset($_SESSION['user'])) {
+          // DISPLAY SETTINGS
+          echo "<br/>";
+        }
+
+      ?>
 
       <!-- MAIN CONTENT GOES HERE -->
+      <div id="profile" class="btn-settings">
+      
+      </div>
+      <div id="font-size" class="btn-settings">
+      
+      </div>
+      <div id="language" class="btn-settings">
+      
+      </div>
+      <div id="feedback" class="btn-settings">
+      
+      </div>
 
       <?php include "partials/navigation.php"; ?>
 
